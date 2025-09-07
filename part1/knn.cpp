5ce8612ca48fd263cc0d994f844f5ca54a118db2
tara-shukla <tara.h.shukla@gmail.com>
2025-09-07 02:52:03 -0400

    initial builder try

--- part1/knn.cpp
+++ part1/knn.cpp
@@ -18,6 +18,7 @@ constexpr float getCoordinate(Embedding_T e, size_t axis)
     return e;  // scalar case
 }
 
+
 // Build a balanced KD‐tree by splitting on median at each level.
 Node* buildKD(std::vector<std::pair<Embedding_T,int>>& items, int depth) {
     /*
@@ -25,7 +26,30 @@ Node* buildKD(std::vector<std::pair<Embedding_T,int>>& items, int depth) {
     You should recursively construct the tree and return the root node.
     For now, this is a stub that returns nullptr.
     */
-    return nullptr;
+
+    if (items.empty()) return nullptr;
+
+    sort(items.begin(), items.end(),
+		[](auto& a, auto& b){
+			return (a.first > b.first);
+		});
+    
+
+    int n = items.size();
+
+    //just doing this should still make the median rule go to the smaller one right?
+    int medianIndex = n/2;
+    
+    std::vector<std::pair<Embedding_T,int>> leftTree(items.begin(), items.begin()+medianIndex);
+    std::vector<std::pair<Embedding_T,int>> rightTree(items.begin()+medianIndex, items.end());
+    
+    Node* root = new Node{items[medianIndex].first, items[medianIndex].second};
+    
+    //build tree
+    root->left = buildKD(leftTree, depth + 1);
+    root->right = buildKD(rightTree, depth + 1);
+    
+    return root;
 }
 
 
